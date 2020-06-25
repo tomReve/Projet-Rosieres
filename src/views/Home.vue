14208c052488729c576dd230c8684f3759ac37a6
Clément TOLOIS <clement.tolois@gmail.com>
2020-06-25 13:50:27 +0200

    Feat: store data in localStorage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,30 +5,48 @@
       <p>Afficher</p>
       <v-select :options="elementsPerPageOptions" :value="elementsPerPage" :searchable="false" @input="elementsPerPageChange"/>
       <p>personnes par page</p>
-      <pagination :page="page" :pages="numberOfPages" @change="paginationChange"/>
     </section>
+    <pagination :page="page" :pages="numberOfPages" @change="paginationChange"/>
     <table>
-      <thead>
-        <template v-for="data in rawData">
-          <tr :key="data.__EMPTY" v-if="data.__EMPTY == 1">
-            <th v-for="(element, index) in data" :key="element.key">{{index}}</th>
+      <thead v-for="data in rawData" :key="data.__EMPTY">
+          <tr v-if="data.__EMPTY == 1">
+            <th>Prénom</th>
+            <th>Nom</th>
+            <th>Genre</th>
+            <th>Pays</th>
+            <th>Âge</th>
+            <th>Date</th>
+            <th>Id</th>
+            <!-- <th v-for="(element, index) in data" :key="element.key">{{index}}</th> -->
           </tr>
-        </template>
+          <tr :key="data.__EMPTY" v-if="data.__EMPTY == 1">
+            <th>
+              <input type="text" v-model="filterFirstName">
+            </th>
+            <th>
+              <input type="text" v-model="filterLastName">
+            </th>
+            <th>
+              <v-select/>
+            </th>
+            <th>
+
+            </th>
+          </tr>
       </thead>
       <tbody>
         <tr v-for="data in pageRawData" :key="data.__EMPTY">
-          <td>{{data['Id']}}</td>
           <td>{{data['First Name']}}</td>
           <td>{{data['Last Name']}}</td>
           <td>{{data['Gender']}}</td>
           <td>{{data['Country']}}</td>
           <td>{{data['Age']}}</td>
           <td>{{data['Date']}}</td>
+          <td>{{data['Id']}}</td>
         </tr>
       </tbody>
     </table>
     <pagination :page="page" :pages="numberOfPages" @change="paginationChange"/>
-    <p>{{ pageRawData }}</p>
   </div>
 </template>
 
@@ -50,14 +68,22 @@
         elementsPerPageOptions:[10,25,50],
         elementsPerPage:null,
         page:1,
-
+        filterFirstName:null,
+        filterLastName:null
       }
     },
     computed:{
       pageRawData(){
         const firstElement = this.page*this.elementsPerPage - this.elementsPerPage;
         const lastElement = this.page*this.elementsPerPage;
-        return this.rawData.slice(firstElement,lastElement);
+        let data = this.rawData.slice(firstElement,lastElement);
+        if(this.filterFirstName) {
+          data = data.filter(element => (element['First Name'].toLowerCase().includes(this.filterFirstName.toLowerCase())));
+        }
+        if(this.filterLastName) {
+          data = data.filter(element => (element['Last Name'].toLowerCase().includes(this.filterLastName.toLowerCase())));
+        }
+        return data;
       },
       numberOfPages(){
         return this.rawData.length/this.elementsPerPage;
@@ -65,10 +91,14 @@
     },
     mounted(){
       this.elementsPerPage = this.elementsPerPageOptions[0];
+      if(localStorage.rawData) {
+        this.rawData = JSON.parse(localStorage.rawData);
+      }
     },
     methods: {
       dataChange(e) {
         this.rawData = e;
+        localStorage.rawData = JSON.stringify(this.rawData);
       },
       elementsPerPageChange(e){
         this.elementsPerPage = e;
