061501b287d56e63e0f3cbafd7939a693ac77ce8
Clément TOLOIS <clement.tolois@gmail.com>
2020-06-25 12:32:41 +0200

    Feat: Add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,133 @@
+<template>
+    <div class="pagination">
+        <div class="pagination-link pagination-previous" :class="{'disabled':isDisabled('previous')}" @click="previousPage">{{ previous }}</div>
+        <div class="pagination-selection">
+            <div v-for="page in pagesToDisplay" :key="page" class="pagination-page-link" :class="{'selected':isSelected(page)}" @click="selectPage(page)">{{ page }}</div>
+        </div>
+        <div class="pagination-link pagination-next" @click="nextPage" :class="{'disabled':isDisabled('next')}">{{ next }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Pagination',
+    props:{
+        page:{
+            type:Number,
+            default:1
+        },
+        pages:{
+            type:Number,
+            default:10
+        },
+        display:{
+            type:Number,
+            default: 5,
+        },
+        previous:{
+            type:String,
+            default: 'Previous'
+        },
+        next:{
+            type: String,
+            default: 'Next'
+        },
+    },
+    data(){
+        return {
+            localPage:null
+        }
+    },
+    computed:{
+        pagesToDisplay(){/* Doit etre amélioré pour fonctionner avec tte les props */
+            const pages = [];
+            if(this.page == 1) {
+                for(let i = this.page; i < this.page+this.display;i++) {
+                    pages.push(i);
+                }
+            } else if(this.page == 2) {
+                pages.push(1);
+                for(let i = this.page; i < this.page+this.display-1;i++) {
+                    pages.push(i);
+                }
+            } else if(this.page == this.pages - 1) {
+                for(let i = this.page-(this.display-2); i <= this.page;i++) {
+                    pages.push(i);
+                }
+                pages.push(this.pages);                
+            } else if(this.page == this.pages) {
+                for(let i = this.page-(this.display-1); i <= this.page;i++) {
+                    pages.push(i);
+                }                
+            } else {
+                for(let i = this.page-(this.display-Math.ceil(this.display/2)); i <= this.page+Math.floor(this.display/2);i++) {
+                    pages.push(i);
+                }              
+            }
+            return pages;
+        }
+    },
+    watch:{
+        page(page) {
+            this.localPage = page;
+        }
+    },
+    methods:{
+        previousPage(){
+            if(this.page > 1) {
+                this.$emit('change',this.page-1);
+            }
+        },
+        nextPage(){
+            if(this.page < this.pages) {
+                this.$emit('change',this.page+1);
+            }
+        },
+        selectPage(page){
+            this.$emit('change',page);
+        },
+        isSelected(page) {
+            if(page == this.page) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        isDisabled(position){
+            if(position == 'previous' && this.page == 1) {
+                return true;
+            } else if(position == 'next' && this.page == this.pages) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+.pagination {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    .pagination-selection {
+        width: 60%;
+        display: flex;
+        justify-content: space-around;
+    }
+
+    .pagination-link, .pagination-page-link {
+        cursor: pointer;
+    }
+
+    .pagination-page-link.selected {
+        font-weight: bolder;
+        color: red;
+    }
+
+    .pagination-link.disabled {
+        color: lightgray;
+    }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <input-xls @change="dataChange" />
+    <section id="pagination">
+      <p>Afficher</p>
+      <v-select :options="elementsPerPageOptions" :value="elementsPerPage" :searchable="false" @input="elementsPerPageChange"/>
+      <p>personnes par page</p>
+      <pagination :page="page" :pages="numberOfPages" @change="paginationChange"/>
+    </section>
     <table>
       <thead>
         <!-- <tr v-for="data in rawData" :key="data.__EMPTY">
@@ -10,7 +16,7 @@
         </tr> -->
       </thead>
       <tbody>
-        <tr v-for="data in rawData" :key="data.__EMPTY">
+        <tr v-for="data in pageRawData" :key="data.__EMPTY">
           <td>{{data['Id']}}</td>
           <td>{{data['First Name']}}</td>
           <td>{{data['Last Name']}}</td>
@@ -21,27 +27,73 @@
         </tr>
       </tbody>
     </table>
-    <p>{{ rawData }}</p>
+    <pagination :page="page" :pages="numberOfPages" @change="paginationChange"/>
+    <p>{{ pageRawData }}</p>
   </div>
 </template>
 
 <script>
-  import InputXLS from '@/components/InputXLS'
+  import InputXLS from '@/components/InputXLS';
+  import Pagination from '@/components/Pagination';
+  import vSelect from 'vue-select'
 
   export default {
     name: 'Home',
     components: {
-      'input-xls': InputXLS
+      'input-xls': InputXLS,
+      'pagination': Pagination,
+      'v-select':vSelect
     },
     data() {
       return {
         rawData: [],
+        elementsPerPageOptions:[10,25,50],
+        elementsPerPage:null,
+        page:1,
+
       }
+    },
+    computed:{
+      pageRawData(){
+        const firstElement = this.page*this.elementsPerPage - this.elementsPerPage;
+        const lastElement = this.page*this.elementsPerPage;
+        return this.rawData.slice(firstElement,lastElement);
+      },
+      numberOfPages(){
+        return this.rawData.length/this.elementsPerPage;
+      }
+    },
+    mounted(){
+      this.elementsPerPage = this.elementsPerPageOptions[0];
     },
     methods: {
       dataChange(e) {
         this.rawData = e;
+      },
+      elementsPerPageChange(e){
+        this.elementsPerPage = e;
+        this.page = 1;
+      },
+      paginationChange(e){
+        this.page = e;
       }
     }
   }
 </script>
+
+<style lang="scss">
+@import "vue-select/src/scss/vue-select.scss";
+.home {
+  width: 100%;
+
+  #pagination {
+    display: flex;
+    align-items: center;
+
+    .v-select {
+      width: 10%;
+      margin: 0 1%;
+    }
+  }
+}
+</style>
